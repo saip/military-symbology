9162ff47cebe18f08c5b3d7887bb939a6d7a7cad
Chris Moore <csmooreacm@acm.org>
2018-09-10 10:10:09 -0400

    Break out view models

---
+++ source/ProSymbolEditor/ViewModels/TabBaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ArcGIS.Desktop.Framework.Contracts;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProSymbolEditor.ViewModels
+{
+    class TabBaseViewModel : PropertyChangedBase
+    {
+
+
+
+
+    }
+}
